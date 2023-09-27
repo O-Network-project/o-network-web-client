e9fd05bb3e0212a933a8e6c82f2a8da4094dee57
Hego-Pierre <mr.p.hego@gmail.com>
2023-09-27 10:26:10 +0200

    fix error message typo in login form

--- src/redux/thunks/user.js
+++ src/redux/thunks/user.js
@@ -22,7 +22,7 @@ export const login = createAsyncThunk("users/login", async (credentials, thunkAp
         if (error.response.status === 403)
             return thunkApi.rejectWithValue({
                 status: 403,
-                message: "Votre compte est desactivée. Veuillez contacté le gérant de l'organisation."
+                message: "Votre compte est désactivé. Veuillez contacter le gérant de l'organisation."
             })
 
         return thunkApi.rejectWithValue({ 
