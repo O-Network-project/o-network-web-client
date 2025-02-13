b56926322a9c54a3ca90408bcbd0170f112b9664
Jon Garbayo <jon.garbayo@gmail.com>
2025-02-13 10:39:39 +0100

    🚨 Disable `import/no-default-export` rule of ESLint in `vite.config.js`
    This rule was added to the ESLint config in a previous commit, but Vite requires a default export in its config file. Disabling this rule in this specific file is the best way to suppress the ESLint error without reducing the overall consistency of the entire app.

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,6 @@
+// Default exports are forbidden in this project, but Vite requires one to work
+// properly
+/* eslint-disable import/no-default-export */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
