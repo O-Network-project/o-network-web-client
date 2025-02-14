9a73fd2f9cf2e1c1f149eebf2788853ef1c4f597
Jon Garbayo <jon.garbayo@gmail.com>
2025-02-14 16:53:59 +0100

    🔧 Modify ESLint configuration to remove "error" or "warning" mentions
    With the `eslint-plugin-only-warn` plugin all ESLint problems are now showed as warnings, so the project rules should only be "on" or "off" to stay consistent.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,37 +12,37 @@ module.exports = {
     settings: { react: { version: '18.2' } },
     plugins: ['only-warn', 'react-refresh'],
     rules: {
-        'no-unused-vars': ['error', {
+        'no-unused-vars': [1, {
             args: 'after-used', // StandardJS set it to none by default
             caughtErrors: 'all', // StandardJS set it to none by default
             ignoreRestSiblings: false, // StandardJS set it to true by default
             vars: 'all'
         }],
-        indent: ['warn', 4, { SwitchCase: 1 }],
-        quotes: ['error', 'single', { allowTemplateLiterals: true }],
-        'import/named': 'error',
-        'import/order': ['error', { 'newlines-between': 'never' }],
-        'import/newline-after-import': 'error',
-        'import/no-useless-path-segments': ['error', { noUselessIndex: true }],
-        'import/no-default-export': 'error',
-        'space-before-function-paren': ['error', {
+        indent: [1, 4, { SwitchCase: 1 }],
+        quotes: [1, 'single', { allowTemplateLiterals: true }],
+        'import/named': 1,
+        'import/order': [1, { 'newlines-between': 'never' }],
+        'import/newline-after-import': 1,
+        'import/no-useless-path-segments': [1, { noUselessIndex: true }],
+        'import/no-default-export': 1,
+        'space-before-function-paren': [1, {
             anonymous: 'always',
             named: 'never',
             asyncArrow: 'always'
         }],
-        'arrow-parens': ['error', 'as-needed'],
-        'react/function-component-definition': 'error',
-        'react/no-unescaped-entities': ['error', { forbid: ['>', '}'] }],
-        'react/jsx-equals-spacing': ['error', 'never'],
-        'react/jsx-tag-spacing': ['error', {
+        'arrow-parens': [1, 'as-needed'],
+        'react/function-component-definition': 1,
+        'react/no-unescaped-entities': [1, { forbid: ['>', '}'] }],
+        'react/jsx-equals-spacing': [1, 'never'],
+        'react/jsx-tag-spacing': [1, {
             closingSlash: 'never',
             beforeSelfClosing: 'always',
             afterOpening: 'never',
             beforeClosing: 'never'
         }],
-        'jsx-quotes': ['error', 'prefer-double'],
+        'jsx-quotes': [1, 'prefer-double'],
         'react-refresh/only-export-components': [
-            'warn',
+            1,
             { allowConstantExport: true }
         ]
     }
