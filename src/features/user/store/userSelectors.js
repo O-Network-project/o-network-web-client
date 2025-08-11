f142eabcfd962cb7071dc8360cd5fa3cb2d5e0bd
Jon Garbayo <jon.garbayo.pro@gmail.com>
2025-08-11 18:12:10 +0200

    ⚰️ Remove unused Redux selectors

--- src/features/user/store/userSelectors.js
+++ src/features/user/store/userSelectors.js
@@ -1,19 +1,13 @@
 export const selectUser = state => state.user
 
-export const selectUserName = state => selectUser(state).name
-
-export const selectUserIsLogged = state => selectUserName(state) !== ''
-
-export const selectUserOrganization = state => selectUser(state).organization
+export const selectUserIsLogged = state => selectUser(state).name !== ''
 
 export const selectUserOrganizationId = state =>
-    selectUserOrganization(state)?.id
+    selectUser(state).organization?.id
 
 export const selectUserOrganizationName = state =>
-    selectUserOrganization(state)?.name
+    selectUser(state).organization?.name
 
 export const selectUserId = state => selectUser(state).id
 
-export const selectUserRole = state => selectUser(state).role
-
-export const selectUserIsAdmin = state => selectUserRole(state) === 'admin'
+export const selectUserIsAdmin = state => selectUser(state).role === 'admin'
