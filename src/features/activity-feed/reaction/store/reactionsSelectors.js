f742c0d7fed9447a8eddbad061aa7261c90d55c8
Jon Garbayo <jon.garbayo.pro@gmail.com>
2025-08-12 19:51:07 +0200

    🚧

--- src/features/activity-feed/reaction/store/reactionsSelectors.js
+++ src/features/activity-feed/reaction/store/reactionsSelectors.js
@@ -2,6 +2,8 @@ import { createSelector } from '@reduxjs/toolkit'
 import { selectPost } from '../../post/store/postsSelectors'
 import { selectUserId } from '../../../user/store/userSelectors'
 import { reactionsAdapter } from './reactionsAdapter'
+import { REACTION_TYPES } from '../data/reactionTypes'
+import { shallowEqual } from 'react-redux'
 
 const reactionsAdapterSelectors = reactionsAdapter.getSelectors(
     state => state.reactions
@@ -16,24 +18,35 @@ export const selectReaction = reactionsAdapterSelectors.selectById
 export const selectPostReactionIds = (state, postId) =>
     selectPost(state, postId).reactionIds
 
-/**
- * @param {Object} state
- * @param {number} postId
- */
-export const selectPostReactions = createSelector(
-    [state => state, selectPostReactionIds],
-    (state, postReactionIds) => postReactionIds.map(
-        reactionId => reactionsAdapterSelectors.selectById(state, reactionId)
-    )
+export const selectPostReactionsCount = (state, postId) =>
+    selectPostReactionIds(state, postId).length
+
+export const makeSelectPostReactionsSelector = () => createSelector(
+    [state => state.reactions.entities, selectPostReactionIds],
+    (reactionEntities, postReactionIds) => postReactionIds.map(reactionId => {
+        console.count(`makeSelectPostReactionsSelector`)
+        return reactionEntities[reactionId]
+    })
 )
 
-/**
- * @param {Object} state
- * @param {number} postId
- */
-export const selectCurrentUserReactionToPost = createSelector(
+export const makeSelectPostReactionTypesSelector = selectPostReactions => createSelector(
+    [selectPostReactions],
+    postReactions => Object.values(REACTION_TYPES).filter(reactionType =>
+        postReactions.some(reaction => {
+            console.count(`makeSelectPostReactionTypesSelector`)
+            return reaction.type === reactionType
+        })
+    ), {
+        memoizeOptions: (a, b) => a === b || shallowEqual(a, b)
+    }
+)
+
+export const makeSelectCurrentUserReactionToPostSelector = selectPostReactions => createSelector(
     [selectUserId, selectPostReactions],
-    (userId, postReactions) => postReactions.find(
-        reaction => reaction.author.id === userId
-    )
+    (userId, postReactions) => postReactions.find(reaction => {
+        console.count(`makeSelectCurrentUserReactionToPostSelector`)
+        return reaction.author.id === userId
+    }), {
+        memoizeOptions: (a, b) => a === b || shallowEqual(a, b)
+    }
 )
